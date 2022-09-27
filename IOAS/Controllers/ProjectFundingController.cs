fc757477e0bf02d9ba6a4f22ccb2528bfea7cbfe
ICSRIT14 <ICSRIT14@icsrpis.iitm.ac.in>
2022-09-27 10:24:15 +0530

    TSA controller and service for development

---
+++ IOAS/Controllers/ProjectFundingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IOAS.Controllers
+{
+    public class ProjectFundingController : Controller
+    {
+        // GET: ProjectFunding
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
