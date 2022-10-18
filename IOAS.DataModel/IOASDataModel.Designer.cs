0fe277c3f0de55241bacde6129978e27130cbcef
ICSR <ICSRIT14@icsrpis.iitm.ac.in>
2022-10-18 11:00:50 +0530

    Edmx changes for TSA

--- IOAS.DataModel/IOASDataModel.Designer.cs
+++ IOAS.DataModel/IOASDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\Application ICSR\Recruitement TULA\Git Tula\ICSRITSoftwareDevelopmentTeam\TULA\IOAS.DataModel\IOASDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\Application ICSR\TSA TULA\ICSRITSoftwareDevelopmentTeam\TULA\IOAS.DataModel\IOASDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
